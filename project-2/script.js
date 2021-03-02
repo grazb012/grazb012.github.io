a44bc29260a4ff45d1c93297d8b8ffba871de30c
grazb012 <grazb012@newschool.edu>
2021-03-02 18:54:22 -0500

    project 2 final

--- project-2/script.js
+++ project-2/script.js
@@ -1,18 +1,18 @@
 let btn = document.getElementById('btn')
 
-let links = [link0, link1, link2, link3, link4, link5, link6, link7, link8, link9]
-	link0 ='sections/one/index.html'
-	link1 ='sections/two/index.html'
-	link2 ='sections/three/index.html'
-	link3 ='sections/four/index.html'
-	link4 ='sections/five/index.html'
-	link5 ='sections/six/index.html'
-	link6 ='sections/seven/index.html'
-	link7 ='sections/eight/index.html'
-	link8 ='sections/nine/index.html'
-	link9 ='sections/ten/index.html'
-
-
+let links = [
+	'sections/one/index.html',
+	'sections/two/index.html',
+	'sections/three/index.html',
+	'sections/four/index.html',
+	'sections/five/index.html',
+	'sections/six/index.html',
+	'sections/seven/index.html',
+	'sections/eight/index.html',
+	'sections/nine/index.html',
+	'sections/ten/index.html'
+]
+		
   function randomLink() {
         var randomLink = Math.round[Math.random() * 9];
  }
