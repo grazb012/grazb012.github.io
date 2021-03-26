a3e18d8fb0230366b48305695f633fda6e1d24a0
grazb012 <grazb012@newschool.edu>
2021-03-26 09:35:45 -0400

    landing page

---
+++ exercise-5/script.js
@@ -0,0 +1,9 @@
+//change color on click
+function colorChange() {
+	document.body.style.backgroundColor = "#fffdd0";
+	document.getElementByClassName('text').style.color = "#161B22"
+}
+/*
+document.querySelector('#button').onclick = function(){
+document.querySelector('body').style.backgroundColor="";
+*/
