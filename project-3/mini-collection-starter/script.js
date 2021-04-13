4ee6fcaafff5b7e3f18b5c01c1d165adff68da88
grazb012 <grazb012@newschool.edu>
2021-04-13 17:49:01 -0400

    project 3 progress

---
+++ project-3/mini-collection-starter/script.js
@@ -0,0 +1,103 @@
+//console.log("is this working?");
+
+//load the airtable library, call it "Airtable";
+var Airtable = require("airtable");
+//console.log(Airtable);
+
+//use airtable library, connect to base using API key
+var base = new Airtable({ apiKey: "keyiFaq9DveHPz98w" }).base(
+  "appBE7XG58Ai7wMyb"
+);
+
+// get our collection base, select all the records
+// specify functions that will receive the data
+base("desserts")
+  .select({})
+  .eachPage(gotPageOfDesserts, gotAllDesserts);
+
+//empty array for album data
+var desserts = [];
+
+//callback function that recieves data
+function gotPageOfDesserts(records, fetchNextPage) {
+  console.log("gotPageOfDesserts()");
+  // add the records from this page to the array
+  desserts.push(...records);
+  // request more pages
+  fetchNextPage();
+}
+
+// call back function that is called when all pages are loaded
+function gotAllDesserts(err) {
+  console.log("gotAllDesserts()");
+
+  // report an error, you'd want to do something better than this in production
+  if (err) {
+    console.log("error loading data");
+    console.error(err);
+    return;
+  }
+
+  // call functions to log and show the books
+  consoleLogDesserts();
+  showDesserts();
+}
+
+// just loop through the books and console.log them
+function consoleLogDesserts() {
+  console.log("consoleLogDesserts()");
+  desserts.forEach(dessert => {
+    console.log("Dessert:", dessert);
+  });
+}
+
+//look through data, create elements
+function showDesserts() {
+  console.log("showDesserts()");
+  desserts.forEach((dessert,i) => {
+
+    //create container 
+    var dessertContainer = document.createElement("div");
+    dessertContainer.classList.add("dessert-container");
+    document.querySelector(".container").append(dessertContainer);
+
+     //add images
+    var dessertImage = document.createElement("img");
+    dessertName.classList.add("image")
+    dessertImage.src = dessert.fields.images[0].url;
+    document.body.append(dessertImage);
+    dessertContainer.append(dessertImage);
+
+    //add dessert name
+		var dessertName = document.createElement("h1");
+    dessertName.classList.add("name")
+		dessertName.innerText = dessert.fields.name;
+		dessertContainer.append(dessertName);
+
+    //add country
+		var country = document.createElement("h1");
+    country.classList.add("country")
+		country.innerText = dessert.fields.country;
+    dessertContainer.append(country);
+
+    //add continent
+    var continent = document.createElement("h1");
+    continent.classList.add("continent")
+    continent.innerText = dessert.fields.continent;
+    dessertContainer.append(continent); 
+
+    //add ingredients
+    var ingredients = document.createElement("h1");
+    ingredients.classList.add("ingredients")
+    ingredients.innerText = dessert.fields.ingredients;
+    dessertContainer.append(ingredients);
+
+//the HTML element you clicked on:
+let element=event.currentTarget;
+//retrieve the number
+let number=element.dataset.image;
+//gets the dessert data corresponding to the image you clicked on
+let currentImage=desserts[number];
+
+	});
+ }
